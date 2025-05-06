5c82f7f234d0e4362eca3edb8a6a43fd1791e193
Prabesh01 <prabesh01@pm.me>
2025-05-07 00:10:59 +0545

    server2/setup.sh - hoh ch partial comp

--- server2/setup.sh
+++ server2/setup.sh
@@ -14,19 +14,22 @@ rm -rf /root/snap
 ## hoh challenges
 # des: rm -rf hoh-challenges
 
+useradd ctf_sandbox_user
+chsh ctf_sandbox_user -s /bin/no-login
+
 ### pip
 # des: pip3 freeze | xargs pip3 uninstall -y
 
 apt install software-properties-common -y
 add-apt-repository universe
 apt update -y
-apt install python3-pip -y
+apt install python3-pip python3-venv libpcap-dev nodejs git -y
 
 #### nginx
 
-systemctl stop apache2 > /dev/null 2>&1
-systemctl disable apache2 > /dev/null 2>&1
-apt remove apache2 -y
+systemctl stop apache2 || true
+systemctl disable apache2 || true
+apt remove apache2 -y || true
 apt install nginx -y
 systemctl enable --now nginx
 
@@ -35,54 +38,47 @@ ln -s /etc/nginx/sites-available/ronb.conf /etc/nginx/sites-enabled/ronb.conf
 nginx -t
 nginx -s reload
 
-### start web
-nohup env PYTHONPATH=/root/rutu/web/ uvicorn main:app --host 0.0.0.0 --port 8000 > /var/log/rutu/web.log 2>&1 &
-
-### run bot.py and server.bot.py
-nohup python3 -u ~/rutu/bot.py >  /var/log/rutu/bot.log 2>&1 &
-nohup python3 -u ~/rutu/server.bot.py 1 >  /var/log/rutu/server.bot.log 2>&1 &
-
-
-## mst
-
-curl https://raw.githubusercontent.com/Prabesh01/icpmap/refs/heads/main/mst-automation/discord_tg_notification.py -o /root/mst/mst.py
-
-#########################################################################################
-
-## k
-
+#### php
 apt install php php-fpm -y
 systemctl enable --now php7.4-fpm
 
-systemctl stop apache2 > /dev/null 2>&1
-systemctl disable apache2 > /dev/null 2>&1
-apt remove apache2 -y
+systemctl stop apache2 || true
+systemctl disable apache2 || true
+apt remove apache2 -y || true
 
-cp /var/www/mamata/nginx.conf /etc/nginx/sites-available/mamata.conf
-ln -s /etc/nginx/sites-available/mamata.conf /etc/nginx/sites-enabled/mamata.conf
+
+### challenges
+
+declare -A chroot_commands=(
+    ["abay"]="/app/.venv/bin/python3 /app/main.py"
+    ["displaced"]="/usr/bin/node /app/main.js"
+    ["gtavi"]="/app/.venv/bin/python3 /app/main.py"
+    ["maltrail"]="/.venv/bin/python3 /server.py"
+    ["probably"]="/app/.venv/bin/python3 /app/main.py"
+    ["templated"]="/app/.venv/bin/python3 /app/main.py"
+)
+for challenge in "${!chroot_commands[@]}"; do
+    cd "/root/hoh-challenges/${challenge}-source"
+    ./"${challenge}.sh"
+    nohup chroot --userspec=ctf_sandbox_user "/root/hoh-challenges/${challenge}-source/${challenge}" ${chroot_commands[$challenge]} &
+done
+
+# non chroot challenges
+nohup python3 /root/hoh-challenges/sanitize/app.py &
+nohup python3 /root/hoh-challenges/tickethub/main.py &
+
+# idiota challenge - my fav
+nohup python3 /root/hoh-challenges/idiota/main.py &
+# nginx conf
+cp /root/hoh-challenges/idiota//idiota.conf /etc/nginx/sites-available/
+ln -s /etc/nginx/sites-available/idiota.conf /etc/nginx/sites-enabled/idiota.conf
 nginx -t
 nginx -s reload
 
-#########################################################################################
-
-## cookfood
-
-git clone https://github.com/Prabesh01/cookfood.git
-pip3 install flask gunicorn
-
-cp /root/cookfood/cookfood.conf /etc/nginx/sites-available/
-ln -s /etc/nginx/sites-available/cookfood.conf /etc/nginx/sites-enabled/cookfood.conf
-nginx -t
-nginx -s reload
-
-cp /root/cookfood/cookfood.service /etc/systemd/system/
-systemctl daemon-reload
-systemctl enable --now cookfood.service
-
-#########################################################################################
-
-## crontab restore
-
-crontab /root/export/crontab.txt
-rm -rf /root/export/
+# php challenges
+mkdir -p /var/www/html/
+# copy inlane, php_is_tricky and magic_has to /var/www/html/
+cp -r /root/hoh-challenges/inlane/* /var/www/html/
+cp -r /root/hoh-challenges/php_is_tricky/ /var/www/html/
+cp -r /root/hoh-challenges/magic_hash/ /var/www/html/login
 
