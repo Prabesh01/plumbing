ea99c5fc0bb1693199a247148a8e792e1a56ca8b
Prabesh01 <prabesh01@pm.me>
2025-05-07 19:03:05 +0545

    server2/setuo.sh: server2 setup

--- server2/setup.sh
+++ server2/setup.sh
@@ -1,5 +1,7 @@
-set -v
+set -x
 set -e
+
+export PS4='\033[4m\033[1;33m[EXEC]\033[0m \033[0m'
 
 apt update -y
 apt upgrade -y
@@ -9,21 +11,27 @@ apt-mark hold snapd
 apt autoremove -y
 rm -rf /root/snap
 
+log_dir="/root/botlogs/"
+mkdir "${log_dir}" || true
+
 #########################################################################################
 
 ## hoh challenges
 # des: rm -rf hoh-challenges
 
-useradd ctf_sandbox_user
+useradd ctf_sandbox_user || true
 chsh ctf_sandbox_user -s /bin/no-login
+apt install libpcap-dev nodejs git -y
 
 ### pip
 # des: pip3 freeze | xargs pip3 uninstall -y
 
-apt install software-properties-common -y
-add-apt-repository universe
-apt update -y
-apt install python3-pip python3-venv libpcap-dev nodejs git -y
+# apt install software-properties-common -y
+# add-apt-repository universe
+# apt update -y
+apt install python3-pip python3-venv -y
+
+pip install flask Flask-SQLAlchemy
 
 #### nginx
 
@@ -33,19 +41,14 @@ apt remove apache2 -y || true
 apt install nginx -y
 systemctl enable --now nginx
 
-cp /root/rutu/web/cw.nginx /etc/nginx/sites-available/ronb.conf
-ln -s /etc/nginx/sites-available/ronb.conf /etc/nginx/sites-enabled/ronb.conf
-nginx -t
-nginx -s reload
-
 #### php
 apt install php php-fpm -y
-systemctl enable --now php7.4-fpm
+installed_php=$(systemctl list-unit-files 'php*-fpm.service' | grep -oP 'php\d+\.\d+-fpm')
+systemctl enable --now "${installed_php}"
 
 systemctl stop apache2 || true
 systemctl disable apache2 || true
 apt remove apache2 -y || true
-
 
 ### challenges
 
@@ -81,4 +84,95 @@ mkdir -p /var/www/html/
 cp -r /root/hoh-challenges/inlane/* /var/www/html/
 cp -r /root/hoh-challenges/php_is_tricky/ /var/www/html/
 cp -r /root/hoh-challenges/magic_hash/ /var/www/html/login
+
+> /etc/nginx/sites-available/default cat <<<"server {
+	listen 80 default_server;
+	listen [::]:80 default_server;
+
+	root /var/www/html;
+	index index.php index.html;
+
+	server_name _;
+
+	location / {
+		try_files \$uri \$uri/ =404;
+	}
+
+	location ~ \.php$ {
+		include snippets/fastcgi-php.conf;
+		fastcgi_pass unix:/run/php/php-fpm.sock;
+		include      fastcgi.conf;
+	}
+}
+"
+
+nginx -s reload
+
+#########################################################################################
+
+## discord-vc-attendance: icpbot/hoh.cote.ws
+
+cd /root/
+mv discord-vc-attendance discord-vc-attendance.bk
+git clone https://github.com/Prabesh01/discord-vc-attendance.git
+cp -r discord-vc-attendance.bk/. discord-vc-attendance/
+rm -rf /root/discord-vc-attendance.bk
+
+### pip
+pip3 install discord.py pytz requests filelock python-dotenv
+
+### run
+nohup python3 -u /root/discord-vc-attendance/bot.py > "${log_dir}"/hoh-bot.log 2>&1 &
+nohup python3 -u /root/discord-vc-attendance/web/app.py > "${log_dir}/hoh-web.log" 2>&1 &
+
+## nginx
+cp /root/discord-vc-attendance/web/nginx.conf /etc/nginx/sites-available/hoh.conf
+ln -s /etc/nginx/sites-available/hoh.conf /etc/nginx/sites-enabled/hoh.conf
+nginx -s reload
+
+#########################################################################################
+
+## ctfd
+
+curl https://gist.githubusercontent.com/Prabesh01/57643d03db9126659870153e893d37c5/raw/6cf9f199d2c456408f344f6ef91d5e65acdfe09a/ctfd.py -o /root/ctfd.py
+
+#########################################################################################
+
+## conan
+
+cd /root/
+mv ctf-discord-bot ctf-discord-bot.bk
+git clone -b multiple-server-support --single-branch https://github.com/Prabesh01/ctf-discord-bot.git
+cp -r ctf-discord-bot.bk/. ctf-discord-bot/
+rm -rf /root/ctf-discord-bot.bk
+
+### pip
+pip3 install django pillow
+
+### run
+nohup python3 -u /root/ctf-discord-bot/ctfdash/manage.py runserver 5050 > "${log_dir}/conan-web.log" 2>&1 &
+nohup python3 -u /root/ctf-discord-bot/bot.py > "${log_dir}/conan-bot.log" 2>&1 &
+
+
+### nginx
+cp /root/ctf-discord-bot/nginx.conf /etc/nginx/sites-available/conan.conf
+ln -s /etc/nginx/sites-available/conan.conf /etc/nginx/sites-enabled/conan.conf
+nginx -s reload
+
+#########################################################################################
+
+## certbot
+apt install certbot python3-certbot-nginx -y
+certbot --nginx -d conan.cote.ws --non-interactive --agree-tos -m prabesh@cote.ws
+certbot --nginx -d hoh.cote.ws --non-interactive --agree-tos -m prabesh@cote.ws
+certbot --nginx -d icpmail.cote.ws --non-interactive --agree-tos -m prabesh@cote.ws
+
+sudo systemctl enable --now  certbot.timer
+
+#########################################################################################
+
+## crontab restore
+
+crontab /root/export/crontab.txt
+rm -rf /root/export/
 
